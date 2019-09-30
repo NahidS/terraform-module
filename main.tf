9b77d1e349772f5cf837c8e82f25abb56d03f7d6
Nahid Seidi <nahid.seidi@adirit.com>
2019-09-30 20:39:38 +0200

    Change bucket name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@
 # Parameter Store Resources
 ################################################################################
 
-resource "aws_ssm_parameter" "parameter-s3-bucket" {
+resource "aws_ssm_parameter" "parameter-s3-bucket-test" {
   description = "AWS S3 bucket to store data"
   name = "/config/alfredpad-api_dev/alfredpad/aws/s3/bucket-name"
   type = "String"
