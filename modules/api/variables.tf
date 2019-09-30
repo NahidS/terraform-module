3ec1e9338c13ed0f9fd2b7447afa7875640dc50d
Nahid Seidi <nahid.seidi@adirit.com>
2019-09-30 19:12:12 +0200

    Initiate modules files

---
+++ modules/api/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_profile" {
+  description = "AWS profile"
+  default = "adirit-dev-admin"
+}
+
+variable "aws_region" {
+  description = "Region to create resources in"
+}
+
+variable "namespace" {
+  default = "alfredpad"
+  description = "Namespace"
+}
+
+variable "stage" {
+  description = "Stage, can be one of {dev,prd,tst}"
+}
+
+variable "bucket_name" {
+  description = "Bucket name"
+}
