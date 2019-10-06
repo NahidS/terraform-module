a72b9a85e9597d71199f226875b2ab65d8f628d4
Nahid Seidi <nahid.seidi@adirit.com>
2019-10-06 14:30:01 +0200

    Convert to TF.12

--- main.tf
+++ main.tf
@@ -60,10 +60,10 @@ resource "aws_ssm_parameter" "parameter-s3-bucket-test" {
   description = "AWS S3 bucket to store data"
   name = "/config/terraform-module/aws/s3/bucket-name"
   type = "String"
-  value = "${var.bucket_name}"
-  tags {
-    Namespace = "${var.namespace}"
-    Stage = "${var.stage}"
+  value = var.bucket_name
+  tags = {
+    Namespace = var.namespace
+    Stage = var.stage
   }
 }
 
@@ -71,10 +71,10 @@ resource "aws_ssm_parameter" "alfredpad-log-level" {
   description = "Log level for alfredpad"
   name = "/config/alfredpad-api/logging/level/com/adirit"
   type = "String"
-  value = "${var.alfredpad-log-level}"
-  tags {
-    Namespace = "${var.namespace}"
-    Stage = "${var.stage}"
+  value = var.alfredpad-log-level
+  tags = {
+    Namespace = var.namespace
+    Stage = var.stage
   }
 }
 
