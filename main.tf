dc250874125429006ce3abb0f523e2470ca2b284
Nahid Seidi <nahid.seidi@adirit.com>
2019-10-06 15:27:49 +0200

    Remove ssm parameters

--- main.tf
+++ main.tf
@@ -59,14 +59,3 @@ module "user-table" {
 ################################################################################
 # Parameter Store Resources
 ################################################################################
-
-resource "aws_ssm_parameter" "parameter-s3-bucket-test" {
-  description = "AWS S3 bucket to store data"
-  name = "/config/terraform-module/aws/s3/bucket-name"
-  type = "String"
-  value = var.bucket_name
-  tags = {
-    Namespace = var.namespace
-    Stage = var.stage
-  }
-}
